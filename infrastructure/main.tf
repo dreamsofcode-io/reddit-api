8bb5637eae22f23d0e2e072dd089576cc1e85963
Elliott Minns <elliott.minns@pm.me>
2023-08-07 22:27:36 -0500

    Infrastructure added

---
+++ infrastructure/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_sqs_queue" "data_queue" {
+  name                       = "reddit-scraper-queue"
+  visibility_timeout_seconds = 30
+
+  tags = {
+    Project   = "reddit-scraper"
+    CreatedBy = "Terraform"
+  }
+}
