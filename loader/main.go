9be48f33de1100f9f490599e5a75b08a3632998a
Elliott Minns <elliott.minns@pm.me>
2023-08-09 22:55:54 -0500

    loader code added

---
+++ loader/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"time"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func addPost(ctx context.Context, post Post, db *Database) error {
+	comments := post.Comments
+	post.Comments = nil
+
+	postData, err := json.Marshal(post)
+	if err != nil {
+		return fmt.Errorf("failed to marshal post: %w", err)
+	}
+
+	commentData, err := json.Marshal(comments)
+	if err != nil {
+		return fmt.Errorf("failed to marshal comments: %w", err)
+	}
+
+	postRecord := PostRecord{
+		ID:        post.ID,
+		Subreddit: post.Subreddit,
+		Timestamp: uint64(post.Timestamp.Unix()),
+		Data:      string(postData),
+	}
+
+	commentRecord := CommentRecord{
+		ID:   post.ID,
+		Data: string(commentData),
+	}
+
+	if err := db.AddCommentRecord(ctx, commentRecord); err != nil {
+		return fmt.Errorf("failed to add comment record: %w", err)
+	}
+
+	if err := db.AddPostRecord(ctx, postRecord); err != nil {
+		return fmt.Errorf("failed to add post record: %w", err)
+	}
+
+	return nil
+}
+
+func handle(ctx context.Context, event events.SQSEvent, db *Database) error {
+	for _, record := range event.Records {
+		var post Post
+		if err := json.Unmarshal([]byte(record.Body), &post); err != nil {
+			return fmt.Errorf("failed to unmarshal post: %w", err)
+		}
+
+		if err := addPost(ctx, post, db); err != nil {
+			return fmt.Errorf("failed to add post: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	timeout := time.Duration(3) * time.Second
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	db, err := NewDatabase(ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	lambda.Start(func(ctx context.Context, event events.SQSEvent) error {
+		return handle(ctx, event, db)
+	})
+}
---
+++ loader/model.go
@@ -0,0 +1,41 @@
+package main
+
+import "time"
+
+type Post struct {
+	ID         string    `json:"id"`
+	Subreddit  string    `json:"subreddit"`
+	DataType   string    `json:"dataType"`
+	DataURL    string    `json:"dataURL"`
+	IsPromoted bool      `json:"isPromoted"`
+	IsGallery  bool      `json:"isGallery"`
+	Title      string    `json:"title"`
+	Timestamp  time.Time `json:"timestamp"`
+	Author     string    `json:"author"`
+	URL        string    `json:"url"`
+	Points     int       `json:"points"`
+	ScrapedAt  uint64    `json:"scrapedAt"`
+	Comments   []Comment `json:"comments,omitempty"`
+}
+
+type Comment struct {
+	ID        string    `json:"id"`
+	Author    string    `json:"author"`
+	Timestamp time.Time `json:"time"`
+	Content   string    `json:"comment"`
+	Points    int       `json:"points"`
+	IsDeleted bool      `json:"isDeleted"`
+	Children  []Comment `json:"children"`
+}
+
+type PostRecord struct {
+	ID        string `dynamodbav:"post_id"`
+	Subreddit string `dynamodbav:"subreddit"`
+	Timestamp uint64 `dynamodbav:"timestamp"`
+	Data      string `dynamodbav:"data"`
+}
+
+type CommentRecord struct {
+	ID   string `dynamodbav:"post_id"`
+	Data string `dynamodbav:"data"`
+}
